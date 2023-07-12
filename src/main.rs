de95d89f69c6354fe605d4719b8d6f3bf2c8716b
Trevor Apple <trevorapple@Trevors-Air.lan>
2023-07-11 23:38:53 -0700

    work on color buffer

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ extern crate sdl2;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+use sdl2::render::TextureAccess;
+use std::mem::size_of;
 use std::time::Duration;
 
 pub fn main() {
@@ -12,9 +15,12 @@ pub fn main() {
     // TODO: handle errors
     let video_subsystem = sdl_context.video().unwrap();
 
+    const WINDOW_WIDTH: u32 = 800;
+    const WINDOW_HEIGHT: u32 = 600;
+
     // TODO: handle errors
     let window = video_subsystem
-        .window("rust-sdl2 demo", 800, 600)
+        .window("rust-sdl2 demo", WINDOW_WIDTH, WINDOW_HEIGHT)
         .position_centered()
         .build()
         .unwrap();
@@ -22,15 +28,27 @@ pub fn main() {
     // TODO: handle errors
     let mut canvas = window.into_canvas().build().unwrap();
 
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
+    let color_buffer: Vec<u32> = Vec::with_capacity(WINDOW_WIDTH as usize * WINDOW_HEIGHT as usize);
+
+    let texture_creator = canvas.texture_creator();
+    let mut texture = match texture_creator.create_texture(
+        None,
+        TextureAccess::Streaming,
+        WINDOW_WIDTH,
+        WINDOW_HEIGHT,
+    ) {
+        Ok(result) => result,
+        Err(_) => todo!(),
+    };
+
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
     canvas.clear();
     canvas.present();
+
     // TODO: handle errors
     let mut event_pump = sdl_context.event_pump().unwrap();
-    let mut i = 0;
     'running: loop {
-        i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+        canvas.set_draw_color(Color::RGB(0, 0, 0));
         canvas.clear();
 
         // process inputs
@@ -48,8 +66,27 @@ pub fn main() {
         // update
         // The rest of the game loop goes here...
 
-        // render
-        canvas.present();
+        // RENDER
+        {
+            let color_buffer_conversion: &[u8] = unsafe {
+                std::slice::from_raw_parts(
+                    color_buffer.as_ptr() as *const u8,
+                    color_buffer.len() * size_of::<u32>(),
+                )
+            };
+
+            // TODO: Error handling
+            let update_result = texture.update(
+                None,
+                color_buffer_conversion,
+                (WINDOW_WIDTH as usize) * size_of::<u32>(),
+            );
+            // TODO: error handling
+            let copy_result = canvas.copy(&texture, None, None);
+            
+            canvas.present();
+        }
+
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 }
