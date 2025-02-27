006e101281641ef027f8a1b0524f11a5d03377c2
Trevor Apple <trappleco@gmail.com>
2025-02-27 07:29:48 -0800

    build from source

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,43 @@
-pub fn main() {}
+extern crate sdl3;
+
+use std::time::Duration;
+
+use sdl3::{event::Event, keyboard::Keycode, pixels::Color};
+
+pub fn main() {
+    let sdl_context = sdl3::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let window = video_subsystem
+        .window("threed_from_scratch", 800, 600)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas();
+
+    canvas.set_draw_color(Color::RGB(0, 255, 255));
+    canvas.clear();
+    canvas.present();
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut i = 0;
+
+    'running: loop {
+        i = (i + 1) % 255;
+        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+        canvas.clear();
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+
+        canvas.present();
+        std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
+}
