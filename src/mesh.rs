1dfb3f445413624197ddeaf801278d5a336fb368
Trevor Apple <trappleco@gmail.com>
2025-03-25 08:53:12 -0700

    started work on loading obj file

--- src/mesh.rs
+++ src/mesh.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::Read;
+
 use crate::triangle::Face;
 use crate::vector::Vector3;
 
@@ -64,3 +67,55 @@ pub const MESH_FACES: [Face; 12] = [
     Face { a: 6, b: 8, c: 1 },
     Face { a: 6, b: 1, c: 4 },
 ];
+
+fn load_obj_mesh(path: &String) -> (Vec<Vector3>, Vec<Face>) {
+    let mut vertices = vec![];
+    let mut faces = vec![];
+
+    let mut file = match File::open(path) {
+        Ok(file) => file,
+        Err(_) => todo!(),
+    };
+
+    let mut contents = String::new();
+
+    match file.read_to_string(&mut contents) {
+        Ok(_) => {}
+        Err(_) => todo!(),
+    }
+
+    for line in contents.lines() {
+        if line.starts_with("v ") {
+            let rest_of_line = &line[2..];
+            let mut elements = [0.0; 4];
+            for (index, element_str) in rest_of_line.split(" ").enumerate() {
+                elements[index] = match element_str.parse() {
+                    Ok(float) => float,
+                    Err(_) => panic!("Unable to convert vertex to float"),
+                };
+            }
+
+            // TODO: what to do with the affine coordinate (w)?
+            vertices.push(Vector3 {
+                x: elements[0],
+                y: elements[1],
+                z: elements[2],
+            });
+        } else if line.starts_with("f ") {
+            let rest_of_line = &line[2..];
+            // most faces are 3 vertices (a triangle), but there is a possibility for a polygon
+            let mut elements: Vec<usize> = Vec::with_capacity(3);
+            for element_str in rest_of_line.split(" ") {
+                let vertex_index: usize = match element_str.parse() {
+                    Ok(vertex_index) => vertex_index,
+                    Err(_) => panic!("Unable to convert index to usize"),
+                };
+                elements.push(vertex_index);
+            }
+        } else {
+            // TODO: don't do anything here yet
+        }
+    }
+
+    (vertices, faces)
+}
