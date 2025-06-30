79caacd78a1ce36f756a26f60626603503d6f0c2
Trevor Apple <trevorapple@Mac.lan>
2025-06-30 09:45:10 -0700

    uv points are included in clipping stage and interpoldated properly

--- src/clipping.rs
+++ src/clipping.rs
@@ -1,5 +1,7 @@
 use crate::{
-    texture::TextureUv, triangle::Triangle, vector::{Vector3, Vector4}
+    texture::TextureUv,
+    triangle::Triangle,
+    vector::{Vector3, Vector4},
 };
 
 pub struct Plane {
@@ -14,6 +16,17 @@ pub struct FrustumPlanes {
     bottom_plane: Plane,
     left_plane: Plane,
     right_plane: Plane,
+}
+
+#[derive(Clone, Default)]
+struct Polygon {
+    vertices: Vec<(Vector3, TextureUv)>,
+}
+
+impl Polygon {
+    fn len(&self) -> usize {
+        self.vertices.len()
+    }
 }
 
 impl FrustumPlanes {
@@ -110,25 +123,23 @@ impl FrustumPlanes {
 }
 
 fn clip_polygon_against_plane(
-    polygon: &Vec<Vector3>,
+    polygon: &Polygon,
     plane: &Plane,
-) -> Vec<Vector3> {
-    if polygon.len() == 0 {
-        return vec![];
-    } else if polygon.len() == 1 {
+) -> Polygon {
+    if polygon.len() < 2 {
         return polygon.clone();
     }
 
     // Vec for tracking vertices of the final polygon that are inside the frustum
-    let mut inside_vertices = Vec::<Vector3>::with_capacity(10);
+    let mut inside_vertices = Vec::<(Vector3, TextureUv)>::with_capacity(10);
 
     // Track the current and previous vertex so that we know if we've crossed a frustum plane
     let mut current_vertex_index = 0;
     let mut previous_vertex_index = polygon.len() - 1;
 
     while current_vertex_index < polygon.len() {
-        let current_vertex = &polygon[current_vertex_index];
-        let previous_vertex = &polygon[previous_vertex_index];
+        let (current_vertex, current_uv) = &polygon.vertices[current_vertex_index];
+        let (previous_vertex, previous_uv) = &polygon.vertices[previous_vertex_index];
 
         let current_dot = Vector3::dot_product(
             &(current_vertex - &plane.position),
@@ -145,22 +156,28 @@ fn clip_polygon_against_plane(
             let t = previous_dot / (previous_dot - current_dot);
 
             let intersection_point =
-                previous_vertex + &(t * (current_vertex - previous_vertex));
+                previous_vertex + &(t * (current_vertex - previous_vertex));            
+            let intersection_uv = TextureUv{
+                u: previous_uv.u + t * (current_uv.u - previous_uv.u),
+                v: previous_uv.v + t * (current_uv.v - previous_uv.v),
+            };
 
             // Insert the intersection point to the list of inside vertices
-            inside_vertices.push(intersection_point);
+            inside_vertices.push((intersection_point, intersection_uv));
         }
 
         // This vertex is on the inside of the frustum
         if current_dot > 0.0 {
-            inside_vertices.push(current_vertex.clone());
+            inside_vertices.push((current_vertex.clone(), current_uv.clone()));
         }
 
         current_vertex_index += 1;
         previous_vertex_index = current_vertex_index - 1;
     }
 
-    inside_vertices
+    Polygon { 
+        vertices: inside_vertices
+    }
 }
 
 pub fn clip_triangle(
@@ -168,11 +185,13 @@ pub fn clip_triangle(
     triangle: Triangle,
 ) -> Vec<Triangle> {
     // Find the resulting polygon from all of the clipping
-    let polygon = vec![
-        Vector3::from_vector4(&triangle.points[0]),
-        Vector3::from_vector4(&triangle.points[1]),
-        Vector3::from_vector4(&triangle.points[2]),
-    ];
+    let polygon: Polygon = Polygon {
+        vertices: vec![
+            (Vector3::from_vector4(&triangle.points[0]), triangle.texel_coordinates[0].clone()),
+            (Vector3::from_vector4(&triangle.points[1]), triangle.texel_coordinates[1].clone()),
+            (Vector3::from_vector4(&triangle.points[2]), triangle.texel_coordinates[2].clone()),
+        ],
+    };
 
     let polygon =
         clip_polygon_against_plane(&polygon, &frustum_planes.left_plane);
@@ -187,7 +206,6 @@ pub fn clip_triangle(
     let polygon =
         clip_polygon_against_plane(&polygon, &frustum_planes.far_plane);
 
-
     // Transform polygon into triangles
     let triangles_after_clipping = {
         let mut triangles_after_clipping: Vec<Triangle> = vec![];
@@ -200,14 +218,21 @@ pub fn clip_triangle(
                 let index1 = index + 1;
                 let index2 = index + 2;
 
+                let (vertex0, uv0) = &polygon.vertices[index0];
+                let (vertex1, uv1) = &polygon.vertices[index1];
+                let (vertex2, uv2) = &polygon.vertices[index2];
                 let new_triangle = Triangle {
                     points: [
-                        Vector4::from_vector3(&polygon[index0]),
-                        Vector4::from_vector3(&polygon[index1]),
-                        Vector4::from_vector3(&polygon[index2]),
+                        Vector4::from_vector3(vertex0),
+                        Vector4::from_vector3(vertex1),
+                        Vector4::from_vector3(vertex2),
+                    ],
+                    texel_coordinates: [
+                        uv0.clone(),
+                        uv1.clone(),
+                        uv2.clone(),
                     ],
                     color: triangle.color,
-                    ..Default::default() // TODO: handle UV coordinates
                 };
 
                 triangles_after_clipping.push(new_triangle);
